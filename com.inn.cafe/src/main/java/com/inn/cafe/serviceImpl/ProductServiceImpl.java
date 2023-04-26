a5c4f16092d17200200bc67b2f9c1ab96b656187
Chathura Prasanga <chathuraprasanga98@gmail.com>
2023-04-26 23:35:27 +0530

    V4.7

--- com.inn.cafe/src/main/java/com/inn/cafe/serviceImpl/ProductServiceImpl.java
+++ com.inn.cafe/src/main/java/com/inn/cafe/serviceImpl/ProductServiceImpl.java
@@ -1,6 +1,8 @@
 package com.inn.cafe.serviceImpl;
 
 import com.inn.cafe.JWT.JwtFilter;
+import com.inn.cafe.POJO.Category;
+import com.inn.cafe.POJO.Product;
 import com.inn.cafe.constants.CafeConstants;
 import com.inn.cafe.dao.ProductDao;
 import com.inn.cafe.service.ProductService;
@@ -25,7 +27,10 @@ public class ProductServiceImpl implements ProductService {
     public ResponseEntity<String> addNewProduct(Map<String, String> requestMap) {
         try {
             if (jwtFilter.isAdmin()){
-                if (validateProductMap(requestMap,false));
+                if (validateProductMap(requestMap,false)){
+                   productDao.save(getProductFromMap(requestMap, false));
+                }
+                return CafeUtils.getResponseEntity(CafeConstants.INVALID_DATA,HttpStatus.BAD_REQUEST);
             }else
                 return CafeUtils.getResponseEntity(CafeConstants.UNAUTHORIZED_ACCESS,HttpStatus.UNAUTHORIZED);
         }catch (Exception ex){
@@ -34,9 +39,33 @@ public class ProductServiceImpl implements ProductService {
         return CafeUtils.getResponseEntity(CafeConstants.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-//    private boolean validateProductMap(Map<String, String> requestMap, boolean validateId) {
-//        if (requestMap.containsKey("name")){
-//            return true;
-//        }else if (!validateId)
-//    }
+
+    private boolean validateProductMap(Map<String, String> requestMap, boolean validateId) {
+        if (requestMap.containsKey("name")) {
+            if (requestMap.containsKey("id") && validateId) {
+                return true;
+            } else if (!validateId) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private Product getProductFromMap(Map<String, String> requestMap, boolean isAdd) {
+        Category category = new Category();
+        category.setId(Integer.parseInt(requestMap.get("categoryId")));
+
+        Product product = new Product();
+        if (isAdd){
+            product.setId(Integer.parseInt(requestMap.get("id")));
+        }else {
+            product.setStatus("true");
+        }
+        product.setCategory(category);
+        product.setName(requestMap.get("name"));
+        product.setDescription(requestMap.get("description"));
+        product.setPrice(Integer.parseInt(requestMap.get("price")));
+        return product;
+    }
+
 }
