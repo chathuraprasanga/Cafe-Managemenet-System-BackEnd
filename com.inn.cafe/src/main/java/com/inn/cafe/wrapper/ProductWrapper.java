d6ac371623875a29b193cd13fa609fc53dd5ccdf
Chathura Prasanga <chathuraprasanga98@gmail.com>
2023-04-27 11:53:28 +0530

    V4.9

--- com.inn.cafe/src/main/java/com/inn/cafe/wrapper/ProductWrapper.java
+++ com.inn.cafe/src/main/java/com/inn/cafe/wrapper/ProductWrapper.java
@@ -1,6 +1,8 @@
 package com.inn.cafe.wrapper;
 
-//@Data
+import lombok.Data;
+
+@Data
 public class ProductWrapper {
 
     Integer id;
@@ -31,59 +33,4 @@ public class ProductWrapper {
         this.categoryName = categoryName;
     }
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Integer getPrice() {
-        return price;
-    }
-
-    public void setPrice(Integer price) {
-        this.price = price;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public Integer getCategoryId() {
-        return categoryId;
-    }
-
-    public void setCategoryId(Integer categoryId) {
-        this.categoryId = categoryId;
-    }
-
-    public String getCategoryName() {
-        return categoryName;
-    }
-
-    public void setCategoryName(String categoryName) {
-        this.categoryName = categoryName;
-    }
 }
